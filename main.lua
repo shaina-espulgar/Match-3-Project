75057d2797977fa50be6b342e99a897bddb223a1
shaina-espulgar <72699933+shaina-espulgar@users.noreply.github.com>
2021-01-22 12:23:53 +0800

    Need to update the sprites

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@
 
 ]]
 
+love.graphics.setDefaultFilter('nearest', 'nearest')
+
 require 'src/Dependencies'
 
 WINDOW_WIDTH = 1280
@@ -12,8 +14,9 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+BACKGROUND_SCROLL_SPEED = 80
+
 function love.load()
-    love.graphics.setDefaultFilter('nearest', 'nearest')
 
     love.window.setTitle('Match Haikyuu')
 
@@ -22,7 +25,8 @@ function love.load()
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
-        resizable = true
+        resizable = true,
+        canvas = true
     })
 
     -- set music to loop and start
@@ -39,8 +43,7 @@ function love.load()
     gStateMachine:change('start')
 
     backgroundX = 0
-    backgroundScrollSpeed = 80
-
+    
     love.keyboard.keysPressed = {}
 end
 
