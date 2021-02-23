951a681c29f9d8f3a382d665fa4473e8ff2ae5cd
Danish Sibulo <72681403+dmdsibulo@users.noreply.github.com>
2021-02-23 18:24:22 +0800

    Update PlayState.lua

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -86,7 +86,13 @@ function PlayState:update(dt)
             self.boardHighlightX = math.min(7, self.boardHighlightX + 1)
             gSounds['select']:play()
         end
-
+        
+        if self.highlightedTile then
+            self:mouseSelect()
+        else
+            self:mouseHighlight()
+        end
+        
         if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
 
             local x = self.boardHighlightX + 1
@@ -102,21 +108,8 @@ function PlayState:update(dt)
                 gSounds['error']:play()
                 self.highlightedTile = nil
             else
-s
-                local tempX = self.highlightedTile.gridX
-                local tempY = self.highlightedTile.gridY
-
                 local newTile = self.board.tiles[y][x]
-
-                self.highlightedTile.gridX = newTile.gridX
-                self.highlightedTile.gridY = newTile.gridY
-                newTile.gridX = tempX
-                newTile.gridY = tempY
-
-                self.board.tiles[self.highlightedTile.gridY][self.highlightedTile.gridX] =
-                    self.highlightedTile
-
-                self.board.tiles[newTile.gridY][newTile.gridX] = newTile
+                self.board:swap(self.highlightedTile.gridX, self.highlightedTile.gridY, newTile.gridX, newTile.gridY)
 
                 Timer.tween(0.1, {
                     [self.highlightedTile] = {x = newTile.x, y = newTile.y},
@@ -124,27 +117,47 @@ s
                 })
 
                 :finish(function()
-                    self:calculateMatches()
+                    self:calculateMatches(self.highlightedTile.gridX, self.highlightedTile.gridY, newTile.gridX, newTile.gridY)
                 end)
             end
+        end
+        if love.keyboard.wasPressed("r") then
+            self.board:resetR()
         end
     end
 
     Timer.update(dt)
+    self.board:update(dt)
+    if self.board:boardCheck() == false then
+
+        self.board:reset()
+    end
 end
 
 
-function PlayState:calculateMatches()
+function PlayState:calculateMatches(x1, y1, x2, y2)
     self.highlightedTile = nil
 
     local matches = self.board:calculateMatches()
-    
+    if matches == false and x1 > 0 then
+            self.board:swap(x1, y1, x2, y2)
+            local tile1 = self.board.tiles[y1][x1]
+            local tile2 = self.board.tiles[y2][x2]
+            Timer.tween(0.1, {
+                [tile1] = {x = tile2.x, y = tile2.y},
+                [tile2] = {x = tile1.x, y = tile1.y}
+            })
+    end
     if matches then
         gSounds['match']:stop()
         gSounds['match']:play()
 
         for k, match in pairs(matches) do
+            for l, tile in pairs(match) do
+                self.score = self.score + tile.variety * 10
+            end
             self.score = self.score + #match * 50
+            self.timer = self.timer + 1
         end
 
         self.board:removeMatches()
@@ -153,7 +166,7 @@ function PlayState:calculateMatches()
 
         Timer.tween(0.25, tilesToFall):finish(function()
 
-            self:calculateMatches()
+            self:calculateMatches(0, 0, 0, 0)
         end)
 
     else
@@ -161,10 +174,104 @@ function PlayState:calculateMatches()
     end
 end
 
+function PlayState:mouseHighlight()
+    local mouseX, mouseY = push:toGame(love.mouse.getPosition())
+
+    -- converting to grid coords
+    local mouseGridX = math.floor((mouseX - self.board.x )/32) + 1
+    local mouseGridY = math.floor((mouseY - self.board.y)/32) + 1
+
+      -- do this only if mouse is inside the board
+     if mouseGridX > 0 and mouseGridY > 0 and mouseGridX < 9  and mouseGridY < 9  then
+        self.boardHighlightX = mouseGridX - 1
+        self.boardHighlightY = mouseGridY - 1
+
+        -- if there is no highlighted tile on the screen, turn selection off
+
+        -- set selection or unhighlight on the board
+        if love.mouse.wasPressed(1) then
+            if self.highlightedTile == nil then
+                self.highlightedTile = self.board.tiles[mouseGridY][mouseGridX]
+            end
+        end
+     end
+
+end
+
+function PlayState:mouseSelect()
+
+    local highX = self.highlightedTile.gridX
+    local highY = self.highlightedTile.gridY
+
+    local mouseX, mouseY = push:toGame(love.mouse.getPosition())
+    local mouseGridX, mouseGridY = math.floor((mouseX - self.board.x )/32) + 1, math.floor((mouseY - self.board.y)/32) + 1
+
+
+    if love.mouse.wasPressed(1) and (highX > 0 and highY > 0 and highX < 9 and highY < 9)  then
+
+        if math.abs(mouseGridX - highX) + math.abs(mouseGridY - highY) <= 1 then
+            local highTile = self.board.tiles[highY][highX]
+            local swapTile = self.board.tiles[mouseGridY][mouseGridX]
+
+            self.board:swap(mouseGridX, mouseGridY, highX, highY)
+
+            Timer.tween(0.1, {
+                [highTile] = {x = swapTile.x, y = swapTile.y},
+                [swapTile] = {x = highTile.x, y = highTile.y}
+            })
+            
+            -- once the swap is finished, we can tween falling blocks as needed
+            :finish(function()
+                self:calculateMatches(mouseGridX, mouseGridY, highX, highY)
+                
+            end)
+
+
+        else
+            self.highlightedTile = nil
+        end
+
+    end
+
+    -- draw
+
+    local drawX = highX - 1
+    local drawY = highY - 1
+    
+    love.graphics.setColor(64, 224, 208, 255)
+    -- draw actual cursor rect
+    love.graphics.setLineWidth(2)       -- draw the 4 highlights orthogonally
+
+    if highX ~= 8 then
+     love.graphics.rectangle('line', (drawX+1) * 32 + (VIRTUAL_WIDTH - 272),             --right box
+        (drawY) * 32 + 16, 32, 32, 4)
+    end
+    
+    if highX ~= 1 then
+    love.graphics.rectangle('line', (drawX - 1) * 32 + (VIRTUAL_WIDTH - 272),           -- left box
+    (drawY) * 32 + 16, 32, 32, 4)
+    end
+
+    if highY ~= 8 then
+    love.graphics.rectangle('line', (drawX) * 32 + (VIRTUAL_WIDTH - 272),               -- bottom box
+    (drawY + 1) * 32 + 16, 32, 32, 4)
+    end
+
+    if highY ~= 1 then
+    love.graphics.rectangle('line', (drawX) * 32 + (VIRTUAL_WIDTH - 272),               -- top box
+    (drawY - 1 ) * 32 + 16, 32, 32, 4)
+    end
+
+    -- reset color to avoid flash
+    love.graphics.setColor(255, 255, 255, 255)
+    
+end
+
 function PlayState:render()
 
     self.board:render()
-
+    self.board:renderParticles()
+    self.board:renderReset()
 
     if self.highlightedTile then
 
@@ -176,6 +283,10 @@ function PlayState:render()
 
         love.graphics.setBlendMode('alpha')
     end
+    
+    if self.highlightedTile then
+        self:mouseSelect()
+        end
 
     if self.rectHighlighted then
         love.graphics.setColor(217/255, 87/255, 99/255, 1)
